39e449ab997ab1fe45ced08bec2d4cbe1e978c82
jonneyhu <849248269@qq.com>
2021-11-28 23:23:12 +0800

    :art:

--- hop/index.js
+++ hop/index.js
@@ -5,23 +5,55 @@ const { parseUnits, formatUnits } = require('ethers/lib/utils')
 const Tx = require('ethereumjs-tx')
 const Web3 = require('web3')
 const fs = require("fs")
+const util = require("util")
+const { addresses } = require('@hop-protocol/core/addresses/kovan')
+const maticUsdc = '0x2791bca1f2de4661ed88a30c99a7a9449aa84174';
+const maticHusdc = '0x9ec9551d4a1a1593b0ee8124d98590cc71b3b09d';
 
+// async function balance(){
+//     const xdaiurl = 'https://rpc.xdaichain.com/';
+//     const xdaichainid = 0x64;
+//     const maticurl = "https://rpc-mainnet.matic.quiknode.pro";
+//     const maticchainid = 0x89;
+//     const web3 = new Web3(maticurl);
+//     var _from = "0xBa7cE7186719B90901c0687ABE5Ca0f2f36fA555";
+//     let balance = await web3.eth.getBalance(_from);
+//     let price = await web3.eth.getGasPrice();
+//     console.log(balance,price);
+// }
+async function getBalance(addr,chain,contract='',decimals=18){
+    if (contract==''){
+        const token = new Token('mainnet',chain,maticUsdc,decimals)
+        const balance = await token.getNativeTokenBalance(addr);
+        console.log(balance.toString());
+    }else{
+        const token = new Token('mainnet',chain,maticUsdc,decimals)
+        const balance = await token.balanceOf(addr);
+        console.log(balance.toString());
+    }
+    
+    
+  
+ }
 
-
-async function swap() {
-    const privateKey = process.env.PRIVATE_KEY
+async function swap(privateKey,amount) {
+    // const privateKey = process.env.PRIVATE_KEY
     const signer = new Wallet(privateKey)
-
     const hop = new Hop('mainnet', signer)
     const bridge = hop.connect(signer).bridge('USDC')
     // send 10 USDC tokens from Polygon -> xDai
-
     const decimals = 6
-    const amountBN = parseUnits('2', decimals)
-    // const amountOut = await bridge.getAmountOut(amountBN, Chain.Ethereum, Chain.xDai)
-    // console.log(amountOut.toBigInt())
+    const amount_s = util.format('%s',amount);
+    const amountBN = parseUnits(amount_s, decimals);
+    const amountOut = await bridge.getAmountOut(amountBN, Chain.Polygon, Chain.xDai)
+    console.log(amountOut.toString())
     const tx = await bridge.send(amountBN, Chain.Polygon, Chain.xDai)
     console.log(tx.hash)
+    setTimeout(async function(){
+        let tx2 = await bridge.send(amountOut.toString(),chain.xDai,chain.Polygon);
+        console.log(tx2.hash);
+    },5000)
+   
 }
 // swap()
 
@@ -59,17 +91,25 @@ function tansfer(from_key,to_addr,amount,is_matic=false) {
 }
 // tansfer()
 
-async function add_liquidity(){
-    const privateKey = process.env.PRIVATE_KEY
+async function add_liquidity(privateKey,amount){
+    // const privateKey = process.env.PRIVATE_KEY
     const signer = new Wallet(privateKey)
 
     const amm = new AMM('mainnet', 'USDC', Chain.xDai,signer);
     const decimals = 6
-    const amountBN = parseUnits('2', decimals)
-    const num = await amm.calculateAddLiquidityMinimum(amountBN,amountBN)
-    console.log(num.toString())
-    // const tx = await amm.addLiquidity(amountBN, amountBN, '0')
-    // console.log(tx.hash)
+    const amount_s = util.format('%s',amount);
+    const amountBN = parseUnits(amount_s, decimals)
+    // const num = await amm.calculateAddLiquidityMinimum(amountBN,amountBN)
+    // console.log(num.toString())
+    const tx = await amm.addLiquidity(amountBN, '0', '0')
+    console.log(tx.hash)
+    setTimeout(async function(){
+        let amountlp = amount*0.97 ;
+        const decimals = 6
+        const amount_s = util.format('%s',amountlp);
+        const amountBN = parseUnits(amount_s, decimals)
+        let tx = await amm.removeLiquidity()
+    },60000)
 }
 // add_liquidity()
 
@@ -77,12 +117,25 @@ async function add_liquidity(){
 async function main(){
     let data = fs.readFileSync("key.txt","utf-8");
     const lines = data.split(/\r?\n/);
-    //转让代币
-    tansfer()
+
+    for(var i=0;i<lines.length;i++){
+        let item = lines[i];
+        let line = item.split(" ") ;
+        let addr = line[0];
+        let key = line[1];
+        console.log(addr,key);
+        // 先完成垮桥，然后添加流动性，移除流动性，最后将代币转移到下一个地址
+        await swap(key)
+    }
+     //转让代币
+    // tansfer()
     //跨桥
-    await swap()
+    // await swap()
     //添加流动性／移除流动性
-    await add_liquidity()
+    // await add_liquidity()
 }
 
-main()
+
+
+// swap('57481c46d76379892a8e9ab74c44b5694850c442ee33ff7ff13fe8e1c63a915f',2)
+getBalance('0x86Fc8F04332446D5779a2bCA82D6cD50FC4e8365',Chain.Polygon,maticUsdc,6)
