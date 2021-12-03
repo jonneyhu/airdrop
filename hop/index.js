dfd1888616deb880d54c056dbc838083accc7af8
jonney <849248269@qq.com>
2021-12-03 15:17:24 +0800

    fix

--- hop/index.js
+++ hop/index.js
@@ -53,57 +53,41 @@ const erc20TransferAbi = [{
     stateMutability: "nonpayable",
     type: "function"
 },]
-async function fromHop() {
-    const privateKey = process.env.PRIVATE_KEY
-    const url = 'https://polygon-rpc.com'
-    // const provider = new HDWalletProvider(privateKey,url)
-    // console.log(provider);
-    const provider = new providers.JsonRpcProvider(url)
-
-    const wallet = new Wallet(privateKey, provider)
-    const signer = wallet.connect(provider)
-    console.log(provider.getSigner())
-    const hop = new Hop('mainnet')
-    const bridge = hop.connect(signer).bridge('USDC')
-    // console.log(chain.provider)
-    // const web3Instance = new Web3(provider)
-    // const swapContract =  new web3Instance.eth.Contract(
-    //     swapAbi,
-    //     '0x5C32143C8B198F392d01f8446b754c181224ac26',
-    //     {gasLimit:'1000000'}
-    // )
-    // const tx = swapContract.methods.swap(1,0,473666,471244,1638775704).send({from:'0xBa7cE7186719B90901c0687ABE5Ca0f2f36fA555'})
-    const decimals = 6
-    const amount_s = util.format('%s', 0.4);
-    const amountBN = parseUnits(amount_s, decimals);
-    const tx = await bridge.execSaddleSwap(Chain.Polygon, false, amountBN, amountBN, 1638348800)
-    console.log(tx.transactionHash);
-}
 
 
-async function swap(privateKey, amount) {
-    // const privateKey = process.env.PRIVATE_KEY
-    const url = 'https://polygon-rpc.com';
+async function send(privateKey,ismatic = true, amount=0) {
+    if (ismatic) {
+        var url = 'https://polygon-rpc.com';
+    } else {
+        var url = 'https://rpc.xdaichain.com/';
+    }
+
     const provider = new providers.JsonRpcProvider(url)
     const signer = new Wallet(privateKey, provider)
     const hop = new Hop('mainnet', signer)
     const bridge = hop.connect(signer).bridge('USDC')
-    const decimals = 6
-    const amount_s = util.format('%s', amount);
-    const amountBN = parseUnits(amount_s, decimals)
-    const tx = await bridge.send(amountBN, Chain.Polygon, Chain.xDai)
-    console.log('send from matic:', tx.hash)
-    sleep(200000)
-    const xdaiurl = 'https://rpc.xdaichain.com/';
-    const provider1 = new providers.JsonRpcProvider(xdaiurl)
-    const signer1 = new Wallet(privateKey, provider1)
-    const hop1 = new Hop('mainnet', signer1)
-    const bridge1 = hop1.connect(signer1).bridge('USDC')
-    let amountBN1 = await getBalance(signer1.address, Chain.xDai, xdaiUsdc, 6)
-    let tx2 = await bridge1.send(amountBN1, chain.xDai, chain.Polygon);
-    console.log('send from xdai', tx2.hash);
 
+    if (ismatic) {
+        const decimals = 6
+        const amount_s = util.format('%s', amount);
+        const amountBN = parseUnits(amount_s, decimals)
+        const tx = await bridge.send(amountBN, Chain.Polygon, Chain.xDai)
+        console.log('send from matic:', tx.hash)
+    } else {
+        let amountBN1 = await getBalance(signer.address, Chain.xDai, xdaiUsdc, 6)
+        let tx2 = await bridge.send(amountBN1, Chain.xDai, Chain.Polygon);
+        console.log('send from xdai', tx2.hash);
+    }
 
+}
+
+async function swap(privateKey, amount) {
+    // const privateKey = process.env.PRIVATE_KEY
+    for(let i=0;i<5;i++){
+        await send(privateKey,true,amount)
+    }
+    sleep(300000)
+    await send(privateKey,false)
 }
 
 async function erc20Transfer(from_key, to_addr, amount) {
@@ -206,7 +190,7 @@ async function add_remove_liquidity(privateKey, amount) {
     const amountBN = parseUnits(amount_s, decimals)
     const tx = await bridge.addLiquidity(amountBN, '0', Chain.Polygon)
     console.log('add_liquidity:', tx.hash)
-    sleep(10000)
+    sleep(20000)
     let amountlp = await getBalance(signer.address, Chain.Polygon, usdcLp, 6);
     let tx1 = await bridge.removeLiquidityOneToken(amountlp, 0, Chain.Polygon)
     console.log('remove_liquidity:', tx1.hash)
@@ -214,12 +198,12 @@ async function add_remove_liquidity(privateKey, amount) {
 }
 
 
-async function main() {
-    let data = fs.readFileSync("key.txt", "utf-8");
-    const lines = data.split(/\r?\n/);
+async function main(lines) {
+    // let data = fs.readFileSync("key.txt", "utf-8");
+    // const lines = data.split(/\r?\n/);
 
     for (var i = 0; i < lines.length; i++) {
-        if (i == 2) {
+        if(i==1){
             break
         }
         let item = lines[i];
@@ -233,19 +217,20 @@ async function main() {
         console.log('[%s] usdc(%s) matic(%s)', addr, usdc_balance.toString(), nativate_balance.toString())
         // 先完成垮桥，然后添加流动性，移除流动性，最后将代币转移到下一个地址
         try {
-            await swap(key, 20);
-            sleep(200000)
-            await add_remove_liquidity(key, 12);
-            sleep(10000)
+            await swap(key, 12);
+            sleep(300000)
+            await add_remove_liquidity(key, 20);
+            sleep(20000)
             await erc20Transfer(key, to_addr);
-            sleep(10000)
+            sleep(20000)
             await nativateTansfer(key, to_addr)
-            sleep(10000)
+            sleep(20000)
             await nativateTansfer(key, to_addr, true)
             const usdc_balance = await getBalance(to_addr, Chain.Polygon, maticUsdc, 6)
             const nativate_balance = await getBalance(to_addr, Chain.Polygon)
             console.log('[%s] usdc(%s) matic(%s)', to_addr, usdc_balance.toString(), nativate_balance.toString())
         } catch (error) {
+            console.log(i)
             throw error
         }
     }
@@ -264,18 +249,31 @@ function sleep(delay) {
 async function test() {
     let data = fs.readFileSync("key.txt", "utf-8");
     const lines = data.split(/\r?\n/);
-    var line1 = lines.slice(12,62);
-    var line2 = lines.slice(62,112);
-    var line3 = lines.slice(112,162);
-    var line4 = lines.slice(162,212);
-    var res=[];
-    res.push()
-    for (let i = 0; i < 10; i++) {
+    var line1 = lines.slice(12, 52);
+    var line2 = lines.slice(52, 92);
+    var line3 = lines.slice(92, 132);
+    var line4 = lines.slice(132, 172);
+    var line5 = lines.slice(172,212);
+    var res = [line1, line2, line3, line4,line5];
+    var initial = [
+        '0xBa7cE7186719B90901c0687ABE5Ca0f2f36fA555 57481c46d76379892a8e9ab74c44b5694850c442ee33ff7ff13fe8e1c63a915f',
+        '0x86Fc8F04332446D5779a2bCA82D6cD50FC4e8365 70e5fbb405e7efabc47d678f3454555ae9b968fa119d8122fd5a2000eba2100d',
+        '0xf4bcCeACcFE4a32B72715BaC6337DB63F6e07869 cc042762a0cfd5a25595bb1f6abc266d8048ab6b432a30a96db3f0e23857f628',
+        '0x1714eF9f6392ca42D069AD7F46Cd63B2bc183764 3fe98e7c9017a295b0c6236715b66d56839d482968425b5527fffc39ab1e16d6',
+        '0x0aAa1Cbcc180Cfe4099a7e749be2b6A37F5edFB2 b4f490811d5fb27c71910014564d1391857a7c456d07c9bfc0ced867bd296d46'
+    ]
+    for (let i = 0; i < res.length; i++) {
+        res[i].unshift(initial[i]);
 
-        // setTimeout(async function () {
-
-
-        // }, 1)
+        setTimeout(async function () {
+            try{
+                await main(res[i])
+            }catch(error){
+                console.log('line:',i)
+                throw error
+            }
+            
+        }, 1)
     }
 
 }
@@ -285,5 +283,5 @@ async function test() {
 // getBalance('0x86Fc8F04332446D5779a2bCA82D6cD50FC4e8365',Chain.Polygon,maticUsdc,6)
 // fromHop()
 // add_liquidity('57481c46d76379892a8e9ab74c44b5694850c442ee33ff7ff13fe8e1c63a915f', 1)
-main()
-// test()
+// main()
+test()
